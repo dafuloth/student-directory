acc58518b59784a1fdbecfc8438fa03215999774
David Thai <davidthai@dafuloth.me>
2019-02-01 02:59:16 +0000

    typos.rb: line 5 array is created by [], not {}

--- typos.rb
+++ typos.rb
@@ -2,7 +2,7 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
-  students = {}
+  students = []
   # get the first name
   name = gets .chomp
   # while the name is not empty, repeat this code
