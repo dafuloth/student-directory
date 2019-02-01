621484eb7a3f8356b4d82159a9c0455476510b0b
David Thai <davidthai@dafuloth.me>
2019-02-01 07:50:45 +0000

    More refactoring. Menu choices, confirm_name, confirm_cohort are extracted into new methods.

--- directory.rb
+++ directory.rb
@@ -2,22 +2,9 @@
 
 def interactive_menu
   loop do
-
-    print_menu
-
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      show_students
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu    
+    selection = gets.chomp    	
+	process(selection)
   end
 end
 
@@ -27,10 +14,23 @@ def print_menu
   puts "9. Exit" # 9 because we'll be adding more items    
 end
 
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
 def show_students
   print_header
-  print(students)
-  print_footer(students)
+  print_student_list
+  print_footer
 end
 
 
@@ -42,9 +42,9 @@ end
 
 # Exercise 1: Modify print(students) to print numbered list of students,
 # e.g. "1. Dr. Hannibal Lecter"
-def print(students)
-  if !students.empty?
-    students.each_with_index do |student, index|
+def print_student_list
+  if !@students.empty?
+    @students.each_with_index do |student, index|
       puts "#{index + 1}.#{index < 9 ? "  " : " "}#{student[:name]} (#{student[:cohort]} cohort)"
 
       # Ex5 - Output the additional information:
@@ -57,17 +57,49 @@ def print(students)
 end
 
 # Ex9. Message now appropriate for 1 student  
-def print_footer(names)
+def print_footer
   # print number of students
-  puts "Overall, we have #{names.count} great student#{names.count == 1 ? "" : "s"}."
+  puts "Overall, we have #{@students.count} great student#{@students.count == 1 ? "" : "s"}."
+end
+
+def confirm_name(name)
+  loop do
+    puts "Storing data for student #{name}."
+    puts "If incorrect, enter correct name now. Otherwise enter #{name} again to confirm."
+    confirmation = gets.chomp
+
+    if confirmation.empty?
+      puts "Name cannot be blank. Terminating."
+      return ""
+    end
+
+    puts
+
+    (name == confirmation) ? (return name) : (name = confirmation)    
+  end
+end
+
+def confirm_cohort(cohort)
+  loop do
+    if cohort.to_s == ""
+      puts "Blank input. Defaulting to current month: #{Time.now.strftime("%B")}."
+      cohort = Time.now.strftime("%B").to_sym
+    end
+
+    puts "Student will be assigned #{cohort} cohort. Enter #{cohort} again to confirm:"
+
+	confirmation = gets.chomp
+
+	puts
+
+    (confirmation.upcase == cohort.to_s.upcase) ? (return cohort) : cohort = confirmation.to_sym
+  end
 end
 
 # Ex7. Modify input_students to accept user input of cohort. Added error checking
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students =[]
 
   puts "Enter the first student name:"
 
@@ -78,52 +110,14 @@ def input_students
   # while the name is not empty, repeat the code
   while !name.empty? do
 
-    confirmation = ""
-
-    loop do
-      puts "Storing data for student #{name}."
-      puts "If incorrect, enter correct name now. Otherwise enter #{name} again to confirm."
-
-      confirmation = gets.chomp
-
-      if confirmation.empty?
-        puts "Name cannot be blank. Terminating."
-        break
-      end
-
-      puts
-
-      if name == confirmation
-        break
-      else
-        name = confirmation
-      end
-    end
-
-    break if confirmation.empty?
+	name = confirm_name(name)
+	
+	break if name.empty?
 
     puts "#{name} must be assigned to a cohort. Please enter cohort."
     cohort = gets.chomp.downcase.to_sym
 
-    loop do
-      if cohort.to_s == ""
-        puts "Blank input. Defaulting to current month: #{Time.now.strftime("%B")}."
-        cohort = Time.now.strftime("%B").to_sym
-      end
-
-    puts "#{name} will be assigned cohort #{cohort}. Enter #{cohort} again to confirm:"
-  
-    confirmation = gets.chomp
-
-    puts
-
-      if confirmation.upcase == cohort.to_s.upcase
-        break
-      else
-        cohort = confirmation.to_sym
-      end
-
-    end
+    cohort = confirm_cohort(cohort)
 
     puts "Who/what to keep this student away from - their nemesis:"
     nemesis = gets.chomp
@@ -135,53 +129,51 @@ def input_students
 
 
     # add the student hash to the array
-    students << {name: name, cohort: cohort, bio: bio, nemesis: nemesis}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort, bio: bio, nemesis: nemesis}
+    puts "Now we have #{@students.count} students"
 
     puts "Enter the next name: "
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
 end
 
 # Exercise 2: Program only prints students whose name begins a specified letter.
-def print_selected(students, begins_with)
+def print_selected(begins_with)
   puts "\nExercise 2: Students whose name begins with #{begins_with.upcase}:"
   # print(students) already exists so reuse the method
-  print(students.select { |student| student[:name][0].upcase == begins_with.upcase })
+  print(@students.select { |student| student[:name][0].upcase == begins_with.upcase })
 end
 
 # Exercise 3: Only print the students whose name is shorter than 12 characters.
-def print_shorter_names(students)
+def print_shorter_names
   puts "\nExercise 3: Students whose names are shorter than 12 characters:"
   # print(students) already exists so reuse the method
-  print(students.select { |student| student[:name].length < 12 })
+  print(@students.select { |student| student[:name].length < 12 })
 end
 
 # Exercise 4: Rewrite the each() method to print all students using while or until 
-def while_print(students)
+def while_print
   puts "\nExercise 4: Same as print(students) but using a while loop:"
 
   counter = 0
 
-  while counter < students.count  
-    puts "\n#{counter + 1}.#{counter < 9 ? "  " : " "}#{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+  while counter < @students.count  
+    puts "\n#{counter + 1}.#{counter < 9 ? "  " : " "}#{@students[counter][:name]} (#{@students[counter][:cohort]} cohort)"
     
     # Ex5 - Output the additional information:
-    puts "Biography: #{students[counter][:bio]}".center(20 + students[counter][:bio].length)
-    puts "Nemesis: #{students[counter][:nemesis]}".center(18 + students[counter][:nemesis].length)
+    puts "Biography: #{@students[counter][:bio]}".center(20 + @students[counter][:bio].length)
+    puts "Nemesis: #{@students[counter][:nemesis]}".center(18 + @students[counter][:nemesis].length)
 
     counter += 1
   end  
 end
 
 # Exercise 5: Adding information: short biography, nemesis (credit to wikipedia and https://villains.fandom.com)
-def print_with_info(students)
+def print_with_info
   puts "\nExercise 5: Added additional student info. Wrote code to output addtional info"
-  if !students.empty?
-    students.each_with_index do |student, index|
+  if !@students.empty?
+    @students.each_with_index do |student, index|
       puts "#{index + 1}.#{index < 9 ? "  " : " "}#{student[:name]} (#{student[:cohort]} cohort)"
       # Ex5 - Output the additional information:
       puts "Biography: #{student[:bio]}".center(20 + student[:bio].length)
