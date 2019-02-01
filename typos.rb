2aa370d6fa73a9b6b8ab319b2f261917d0b654a3
David Thai <davidthai@dafuloth.me>
2019-02-01 03:52:46 +0000

    typos.rb: now seems to be all working

--- typos.rb
+++ typos.rb
@@ -11,30 +11,29 @@ def input_students
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = get.chomp
+    name = gets.chomp
   end
   # return the array of students
   students
 end
 
-def print-header
-  put "The students of my cohort at Makers Academy'
-  puts ``-------------``
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
 def print(students)
-  student.each do /student/
-    puts '#[student{:name}] (#[student{:cohort}] cohort)'
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer names
-  prints "Overall, we have {names.count} great students"
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
 end
 
 
-students = input-students
+students = input_students
 print_header
-print{students}
-print_footer[students]
-
+print(students)
+print_footer(students)
