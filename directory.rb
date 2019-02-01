acabdd887b441b409a28802728401e4105b24069
David Thai <davidthai@dafuloth.me>
2019-02-01 05:39:18 +0000

    Refactoring. students array now accessible by all methods

--- directory.rb
+++ directory.rb
@@ -1,10 +1,10 @@
+@students = []
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
+
+    print_menu
+
     # 2. read the input and save it into a variable
     selection = gets.chomp
     # 3. do what the user has asked
@@ -12,15 +12,25 @@ def interactive_menu
     when "1"
       students = input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
     end
   end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items    
+end
+
+def show_students
+  print_header
+  print(students)
+  print_footer(students)
 end
 
 # put students into array
